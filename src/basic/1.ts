25c1a0e52a82de0797b1d3fb57993b0b56c336d4
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2024-08-23 22:11:17 +0200

    Update 1.ts

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -2,7 +2,7 @@ let age:number = 50;
 let username:string = 'Max';
 let toggle:boolean = true;
 let empty:null = null;
-let callback = (a: number): string => { 
-    return (100 + a).toString();
+let callback = (a: number): number => { 
+    return 100 + a;
 };
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
